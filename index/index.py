431b94fad5c3fdd89c047e35d0ca3b07a550ef62
Aishwarya Hastak <h4hastak@gmail.com>
2024-12-17 00:25:03 -0500

    added bm25 index creation

--- index/index.py
+++ index/index.py
@@ -3,19 +3,23 @@
 import argparse
 import glob
 import torch
+import pickle
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings 
 from langchain.vectorstores import FAISS  
+from langchain.retrievers import BM25Retriever
 
 class Index:
     def __init__(self, 
         model_name = "sentence-transformers/all-mpnet-base-v2",
         chunk_size = 1000, 
         chunk_overlap = 100,
-        vector_db_name = "vector_db"
-        ):
+        k = 4,
+        vector_db_name = "vector_db",
+        keyword_db_name = "keyword_db"
+        ): 
         """
         Initialize the Index class with the given parameters.
 
@@ -25,12 +29,19 @@ class Index:
         :type chunk_size: int
         :param chunk_overlap: The overlap between the chunks
         :type chunk_overlap: int
+        :param k: The number of documents to return from keyword search
+        :type k: int
         :param vector_db_name: The name of the vector database to save the index to
         :type vector_db_name: str
+        :param keyword_db_name: The name of the keyword database to save the index to
+        :type keyword_db_name: str
         """
+        
         # Initialize text splitter parameters
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
+        # Number of documents to return from keyword search
+        self.k = k
 
         # Initialize embedding model
         model_name = model_name
@@ -38,6 +49,7 @@ class Index:
         self.embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
 
         self.vector_db_name = vector_db_name
+        self.keyword_db_name = keyword_db_name
 
     def read_pdfs(self):
         """
@@ -60,19 +72,21 @@ class Index:
         print(f'[DEBUG] Loaded {len(documents)} files') 
         return documents
 
-    def create_index(self):#, documents):
-        """
-        Creates a FAISS vector database from all PDF files in the ./data directory.
-        
-        The function first loads all PDF files using PyPDFLoader, then applies the
-        RecursiveCharacterTextSplitter to split each document into chunks of the specified
-        size with the specified overlap. The chunks are then embedded using the specified
-        HuggingFace model and the FAISS vector database is created and saved to the
-        specified folder. The saved vector database can then be loaded for querying later.
-        
-        :return: The created FAISS vector database
-        """
+    def create_index(self): 
         # Load PDF files
+        """
+        Creates a FAISS vector database and a keyword database from all PDF files
+        in the ./data directory.
+
+        The function first loads all PDF files using PyPDFLoader, then applies the
+        RecursiveCharacterTextSplitter to split each document into chunks of the
+        specified size with the specified overlap. The chunks are then embedded
+        using the specified HuggingFace model and the FAISS vector database is
+        created and saved to the specified folder. The saved vector database can
+        then be loaded for querying later.
+
+        :return: The created FAISS vector database and keyword database
+        """
         documents = self.read_pdfs()
         # Initialize text splitter
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
@@ -85,21 +99,40 @@ class Index:
 
         vector_db.save_local(folder_path = self.vector_db_name, index_name = self.vector_db_name) 
         print(f'[DEBUG] Vector database saved to {self.vector_db_name}')
-        return vector_db
+
+        keyword_db = BM25Retriever.from_documents(all_splits, k=self.k)
+
+        return vector_db, keyword_db
     
-    def load_index(self):
+    def load_index(self): 
+        # Load vector database
         """
-        Tries to load a previously saved FAISS vector database from the specified
-        vector_db_name.  
-        
-        :return: None
+        Loads a previously saved FAISS vector database and keyword database from the specified
+        folder paths.
+
+        The method attempts to load both the vector database and the keyword database using
+        the folder and index names specified by `vector_db_name` and `keyword_db_name`
+        attributes respectively. If loading fails for any reason, an error message is 
+        printed with the exception details.
+
+        :return: A tuple containing the loaded vector database and keyword database.
+        :rtype: tuple
         """
         try:
             vector_db = FAISS.load_local(folder_path = self.vector_db_name, index_name = self.vector_db_name, embeddings = self.embeddings)
             print(f'[DEBUG] Vector database loaded from {self.vector_db_name}')
         except Exception as e:
             print(f'[DEBUG] Vector database could not be loaded from {self.vector_db_name} due to the following error: {str(e)}')
-        return vector_db
+        
+        # Load keyword database
+        try:
+            with open(f"{self.keyword_db_name}.pkl", 'rb') as file_path:
+                keyword_db = pickle.load(file_path) 
+            print(f'[DEBUG] Keyword database loaded from {self.keyword_db_name}')
+        except Exception as e:
+            print(f'[DEBUG] Keyword database could not be loaded from {self.keyword_db_name} due to the following error: {str(e)}')
+
+        return vector_db, keyword_db
     
 
 def main():
@@ -113,8 +146,12 @@ def main():
                         help="Size of chunks for splitting text (default: 1000).")
     parser.add_argument("--chunk_overlap", type=int, default=100, 
                         help="The overlap between chunks (default: 100).")
+    parser.add_argument("--k", type=int, default=4, 
+                        help="Number of documents to return for keyword retriever (default: 4).")
     parser.add_argument("--vector_db_name", type=str, default="vector_db", 
                         help="The name for saving the vector database (default: vector_db).")
+    parser.add_argument("--keyword_db_name", type=str, default="keyword_db", 
+                        help="The name for saving the keyword database (default: keyword_db).")
     parser.add_argument("--load", action="store_true", 
                         help="If set, it will attempt to load an existing vector database instead of creating a new one.")
     
